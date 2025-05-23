ede6583f6cdbf5b33b8c8db474ed664df6c33a14
Kerri Morris <kkmorrisfam@hotmail.com>
2025-05-23 16:57:47 -0700

    Add error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,10 +29,16 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql `INSERT INTO invoices (customer_id, amount, status, date)
+    try {
+        await sql `INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    } catch (error) {
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
+    //call redirect after try/catch because it works by throwing an error
+    //and would be caught in the catch block.  This way it won't run if catch block runs
     redirect('/dashboard/invoices');
 }
 
@@ -44,15 +50,25 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
     const amountInCents = amount * 100;
 
-    await sql`UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}`;
+    try {
+        await sql`UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}`;    
+    } catch (error) {
+        console.error(error);
+    }    
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+        throw new Error('Failed to Delete Invoice')
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');        
+    } catch (error) {
+        console.error(error);
+    }
+
 }
