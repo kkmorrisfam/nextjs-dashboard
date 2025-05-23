8f0d4175f8dfb5670185451f56ed8bbe0765e6b0
Kerri Morris <kkmorrisfam@hotmail.com>
2025-05-22 19:45:07 -0700

    Create invoice, post data to server

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
@@ -13,11 +18,19 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true});
 
 export async function createInvoice(formData: FormData) {
-    
-    const rawFormData = {
+
+    const {customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
-    console.log(rawFormData);
+    });
+    //convert amount into cents to eliminate Javascript floating point errors
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql `INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
