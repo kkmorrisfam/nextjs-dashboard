da92a3674e2a92161da081b6e5e813d3b323eb7c
Kerri Morris <kkmorrisfam@hotmail.com>
2025-05-23 19:56:10 -0700

    fix login-form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -14,7 +14,7 @@ import { useSearchParams } from 'next/navigation';
 
 export default function LoginForm() {
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get('callbackUrl' || '/dashboard');
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
@@ -71,8 +71,12 @@ export default function LoginForm() {
         <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
-           {errorMessage && (
+        <div 
+          className="flex h-8 items-end space-x-1" 
+          aria-live="polite" 
+          aria-atomic="true"
+          >
+          {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
